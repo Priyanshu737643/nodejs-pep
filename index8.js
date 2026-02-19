7e4bb4730bacebc4dbbb2b8ceeefd336ad046c73
{PRIYANSHU <priyanshu.sinha12345@gmail.com>
2026-02-19 16:44:25 +0530

    update

--- index8.js
+++ index8.js
@@ -4,44 +4,42 @@ app.listen(8080, () => {
   console.log("server running");
 });
 
-app.get("/", (req, res) => {
-    const users = [
-      {
-        name: "John",
-        email: "john@gmail.com",
-        role: "student",
-      },
-      {
-        name: "Cathy",
-        email: "cathy@gmail.com",
-        role: "student",
-      },
-      {
-        name: "Admin",
-        email: "admin@gmail.com",
-        role: "admin",
-      },
-    ];
+//? Middleware
+app.use(express.json());
+
+let users = [
+  {
+    id: 1,
+    name: "John",
+    email: "john@gmail.com",
+    role: "student",
+  },
+  {
+    id: 2,
+    name: "Cathy",
+    email: "cathy@gmail.com",
+    role: "student",
+  },
+  {
+    id: 3,
+    name: "Admin",
+    email: "admin@gmail.com",
+    role: "admin",
+  },
+];
+
+app.post("/", (req, res) => {
+  // console.log(req.body);
+  const user = req.body;
+  users.push(user);
   res.json(users);
 });
 
 app.get("/:id", (req, res) => {
-    const users = [
-      {
-        name: "John",
-        email: "john@gmail.com",
-        role: "student",
-      },
-      {
-        name: "Cathy",
-        email: "cathy@gmail.com",
-        role: "student",
-      },
-      {
-        name: "Admin",
-        email: "admin@gmail.com",
-        role: "admin",
-      },
-    ];
-    res.json(users[req.params.id]);
-})
+    const user = users.find((user) => user.id === Number(req.params.id));
+    if (user) {
+        res.send(user);
+    }
+});
+
+
