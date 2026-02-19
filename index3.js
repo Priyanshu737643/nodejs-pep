f7c196d15873d708a9f53480cf76ac9f8381304a
{PRIYANSHU <priyanshu.sinha12345@gmail.com>
2026-02-19 11:31:14 +0530

    update

--- index3.js
+++ index3.js
@@ -4,6 +4,11 @@ app.listen(8080, () => {
     console.log("server running");
 })
 
+app.get("/", (req, res) => {
+  console.log(req.url);
+  res.send("response from server for /");
+});
+
 app.get("/home", (req, res) => {
     res.send("response from server for /home")
 })
